3f5797228f67e4def86e600d4eec0eb44f9a9a63
Graham Eglit <graham.eglit@us-0000517-mac.attlocal.net>
2022-08-19 09:59:03 -0700

    add mod08 script

---
+++ scripts/mod08_lecture.R
@@ -0,0 +1,20 @@
+require(tidyverse)
+require(palmerpenguins)
+require(here)
+
+
+
+
+# Read in Data ------------------------------------------------------------
+
+dat_problem <- read_csv("/Users/graham.eglit/Documents/R_Course/Intro_to_R/data/problem.csv") # note my reletive path will be different from yours
+
+dat_problem <- read_csv(here("data", "problem.csv")
+                        
+dat_problem <- read_csv(here("data", "problem.csv"),
+                        na = c("?", "MD", "-999"),
+                        skip = 3,
+                        col_names = c("ID", "sex", "ethnicity", "treatment", "anxiety", "NA"))
+
+dat_problem %>% 
+  drop_na() # this function drops all rows that contain only NA values
